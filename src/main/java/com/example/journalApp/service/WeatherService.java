ad7d0688f53b9f8ce78ee41b1cadda163bf5cfa5
samruddhi1499 <samchavanxiv9@gmail.com>
2024-05-03 15:27:23 -0400

    ExternalAPi Commit

--- src/main/java/com/example/journalApp/service/WeatherService.java
+++ src/main/java/com/example/journalApp/service/WeatherService.java
@@ -2,22 +2,25 @@ package com.example.journalApp.service;
 
 import com.example.journalApp.apiResponse.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
     @Autowired
     private RestTemplate restTemplate;
-    private static final String APIKEY = "17c144ea0f30a0a42cc07fb01d15e7f6";
+    @Value("${weather.api.key}")
+    private String apikey;
     private static final String API = "https://api.openweathermap.org/data/2.5/find?q=CITY&lang=fr&appid=APIKEY&units=metric";
 
     public WeatherResponse getWheather(String city){
 
-        String finalAPI = API.replace("CITY", city).replace("APIKEY", APIKEY);
+        String finalAPI = API.replace("CITY", city).replace("APIKEY", apikey);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
